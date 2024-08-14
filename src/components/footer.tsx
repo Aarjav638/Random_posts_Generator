7b30b19bb7a76c339a4103ccbb827fcac3321368
Aarjav Jain <anshjain638@gmail.com>
2024-08-14 14:20:58 +0530

    added title in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 const Footer: React.FC = () => {
-    return (
-        <footer className="w-full bg-teal-500 p-8 flex flex-col sm:flex-row justify-between items-center">
-            <p className="text-center text-white font-semibold text-sm">
-                © 2024 Random Quotes Generator. All rights reserved.
-            </p>
-            <div className="w-[25%]">
-                <ul className="flex items-center justify-center gap-x-4 text-white">
-                    <li><Link href="/" referrerPolicy="no-referrer" >Home</Link></li>
-                    <li><Link href="/about" referrerPolicy='no-referrer'>About</Link></li>
-                    <li><Link href="/contact" referrerPolicy='no-referrer'>Contact</Link></li>
-                </ul>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="w-full bg-teal-500 p-8 flex flex-col sm:flex-row justify-between items-center">
+      <p className="text-center text-white font-semibold text-sm">
+        © 2024 Random Post Generator. All rights reserved.
+      </p>
+      <div className="w-[25%]">
+        <ul className="flex items-center justify-center gap-x-4 text-white">
+          <li>
+            <Link href="/" referrerPolicy="no-referrer">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" referrerPolicy="no-referrer">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" referrerPolicy="no-referrer">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
